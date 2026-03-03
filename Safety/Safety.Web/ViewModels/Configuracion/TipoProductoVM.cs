711c11ab7b89da72076090099225a7fd4210094a
emucino <emucino@SIAPTILAP16>
2026-03-03 17:25:04 -0600

    - se agrega Mapster para el uso de automapeo entre los ViewModels y las entidades que se van a utlizar.
    feat
    - se agrega TipoProductoVM

---
+++ Safety/Safety.Web/ViewModels/Configuracion/TipoProductoVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Safety.Web.ViewModels.Configuracion;
+
+public class TipoProductoVM
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    [MaxLength(100)]
+    public string Nombre { get; set; } = string.Empty;
+
+    [MaxLength(250)]
+    public string? Descripcion { get; set; }
+}
